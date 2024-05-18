0975f6cc1417031842be5e008f14bf4141e6890b
faultx01 <trihermwan13@gmail.com>
2024-05-18 22:13:54 +0700

    Update MIUI_12.x.x-Debloater.sh

--- MIUI_12.x.x-Debloater.sh
+++ MIUI_12.x.x-Debloater.sh
@@ -54,4 +54,8 @@ adb shell pm uninstall --user 0 com.amazon.appmanager
 adb shell pm uninstall --user 0 com.miui.android.fashiongallery
 adb shell pm uninstall --user 0 com.android.providers.partnerbookmarks
 adb shell pm uninstall --user 0 com.xiaomi.glgm
+
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.google.apps.youtube.music
 echo "------ [ Done ] ------"
