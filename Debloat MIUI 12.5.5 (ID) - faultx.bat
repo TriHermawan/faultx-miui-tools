f4e2fe89d0fe03c709a0024e308e5504324314f2
faultx01 <trihermwan13@gmail.com>
2025-01-22 14:33:45 +0700

    Update Debloat MIUI 12.5.5 (ID) - faultx.bat

--- Debloat MIUI 12.5.5 (ID) - faultx.bat
+++ Debloat MIUI 12.5.5 (ID) - faultx.bat
@@ -106,6 +106,7 @@ adb shell pm uninstall --user 0 com.xiaomi.glgm
 adb shell pm uninstall --user 0 com.google.android.apps.tachyon
 adb shell pm uninstall --user 0 com.google.android.videos
 adb shell pm uninstall --user 0 com.google.android.apps.youtube.music
+adb shell pm uninstall --user 0 com.miui.daemon
 echo All done.
 PAUSE
 exit
