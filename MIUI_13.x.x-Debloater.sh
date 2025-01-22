8b6166ae3a8f4860802b41cdb6fe9895d8490f98
faultx01 <trihermwan13@gmail.com>
2025-01-22 14:37:43 +0700

    Update MIUI_13.x.x-Debloater.sh

--- MIUI_13.x.x-Debloater.sh
+++ MIUI_13.x.x-Debloater.sh
@@ -56,6 +56,8 @@ adb shell pm uninstall --user 0 com.google.android.videos
 adb shell pm uninstall --user 0 com.google.android.apps.youtube.music
 adb shell pm uninstall --user 0 com.mi.globalminusscreen
 adb shell pm uninstall --user 0 com.miui.daemon
+adb shell pm uninstall --user 0 com.miui.cleanmaster
+adb shell pm uninstall --user 0 com.miui.cleaner
 
 # Miui.EU
 
